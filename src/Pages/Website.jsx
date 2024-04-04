06d5e4b104bb8487529d4d4df1b0d70568945f80
Subh <subhamnegi1396@gmail.com>
2024-04-04 11:25:22 +0530

    created event slider and youthfest page

--- src/Pages/Website.jsx
+++ src/Pages/Website.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import EventsCard from '../components/EventsCard/EventsCard'
+import EventSlider from '../components/EventSlider/EventSlider'
 
 const Website = () => {
     return (
