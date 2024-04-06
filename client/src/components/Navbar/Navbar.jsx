be9271e101379e6f0eab447a72250cd6c06fbcd7
Subh <subhamnegi1396@gmail.com>
2024-04-06 13:14:12 +0530

    added links to navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">YOUNITE</div>
+      <div className="logo"><Link to={"/"}>YOUNITE</Link></div>
       <ul className="nav-links">
-        <li>YOUTHFEST</li>
-        <li>EVENTS</li>
-        <li>ABOUT US</li>
-        <li>CONTACT US</li>
+        <li><Link to={"/youthfest"}>YOUTHFEST</Link></li>
+        <li><Link to={"/events"}>EVENTS</Link></li>
+        <li><Link to={"/about-us"}>ABOUT US</Link></li>
+        <li><Link to={"/contact-us"}>CONTACT US</Link></li>
       </ul>
     </nav>
   );
