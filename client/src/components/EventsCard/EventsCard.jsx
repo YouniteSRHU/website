f2aac3303dd5281b6e766b5934133745dcb27d32
Subh <subhamnegi1396@gmail.com>
2024-04-06 13:56:05 +0530

    adjusted youth fest, navbar and footer padding

--- client/src/components/EventsCard/EventsCard.jsx
+++ client/src/components/EventsCard/EventsCard.jsx
@@ -3,15 +3,12 @@ import './EventCard.css'
 
 const EventsCard = ({ card }) => {
     return (
-
-        <a href={card.eventURL} target="_blank">
-        <div className="item active" style={{ backgroundImage: `url(${card.urlImage}) `}}>
+        <div className="item active" style={{ backgroundImage: `url(${card.urlImage}) ` }}>
             <div className="item-desc">
                 <h3>{card.title}</h3>
                 <p>{card.desc}</p>
             </div>
         </div>
-        </a>
     )
 }
 
