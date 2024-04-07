23d8a7560840b370f0dbab56333bfe7f55d50b7d
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-04-08 00:23:57 +0530

    Added api routes for youthfest and individual event pages

---
+++ server/controllers/eventDataControllers.js
@@ -0,0 +1,50 @@
+import prisma from "../DB/db.config.js";
+
+export const specificEventController = async (req, res) => {
+  const eventData = await prisma.event.findUnique({
+    where: {
+      event_id: Number(req.params.eventId),
+    },
+    include: {
+      images: {
+        select: {
+          image_src: true,
+        },
+      },
+      guidelines: {
+        select: {
+          guideline: true,
+        },
+      },
+      tc: {
+        select: {
+          tc: true,
+        },
+      },
+    },
+  });
+  res.json(eventData);
+};
+
+export const youthFestPageController = async (req, res) => {
+  const pageData = await prisma.fest.findUnique({
+    where: {
+      fest_id: 1,
+    },
+    include: {
+      events: {
+        select: {
+          event_id: true,
+          event_name: true,
+          event_desc: true,
+          images: { 
+            select: {
+                image_src: true,
+            } 
+        },
+        },
+      },
+    },
+  });
+  res.json(pageData);
+};
