168a347349fb0bb1db2c5d2ad18e58ff7f29f740
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-04-12 13:48:13 +0530

    feat/updated-navbar-for-mobile-and-tablet

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from '/logo.png'
+import srhulogo from '/srhulogo.png'
 import Sidebar from '../sidebar/Sidebar'
 import "./Navbar.css";
 
@@ -10,6 +11,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to={"/"}> <img src={logo} alt="logo" width={75} className="logo"/></Link>
+      <Link to={"/"}> <img src={srhulogo} alt="logo"  className="srhulogo"/></Link>
       <ul className={`nav-links ${showMenu ? "show" : ""}`}>
         <li><Link to={"/YOUTHFEST"}>YOUTHFEST</Link></li>
         <li><Link to={"/ABOUT-US"}>ABOUT US</Link></li>
