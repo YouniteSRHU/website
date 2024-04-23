f11e4be83ab26150c0f0160a43de5a47b52c8a74
Subh <subhamnegi1396@gmail.com>
2024-04-24 00:04:55 +0530

    bugs fixed event card link and haeding, date / time format, footer icons and responsiveness

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import "./Footer.css"; 
-
+import "./Footer.css";
+import youtube from '../../assets/icons8-youtube.svg'
+import insta from '../../assets/icons8-instagram.svg'
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>©️ Younite. All rights reserved.</p>
+        <p>©️ Younite SRHU. All rights reserved.</p>
         </div>
         <div className="social-media">
-          <a href="#">Instagram</a>
-          <a href="#">YouTube</a>
+          <a href="https://www.instagram.com/younite_srhu/" target="blank"><img src={insta} alt="icon" className="social-icons"/><span>Instagram</span></a>
+          <a href="https://www.youtube.com/@swamiramahimalayanuniversi7746" target="blank"><img src={youtube} alt="icon" className="social-icons"/>YouTube</a>
         </div>
     </footer>
   );
