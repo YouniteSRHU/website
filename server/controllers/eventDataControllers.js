fe65e4c736cb422c6fcfa0e511a46ef1a1ca5bbe
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-04-25 13:38:02 +0530

    Added image sorting

--- server/controllers/eventDataControllers.js
+++ server/controllers/eventDataControllers.js
@@ -7,6 +7,7 @@ export const specificEventController = async (req, res) => {
     },
     include: {
       images: {
+        orderBy: image_id,
         select: {
           image_src: true,
         },
@@ -40,6 +41,7 @@ export const youthFestPageController = async (req, res) => {
           event_desc: true,
           fest_identifier: true,
           images: {
+            orderBy: image_id,
             select: {
               image_src: true,
             }
