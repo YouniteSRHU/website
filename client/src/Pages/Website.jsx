483db5d01fb715011578fe51263e6c9bc3d980b3
Subh <subhamnegi1396@gmail.com>
2024-04-25 12:53:54 +0530

    added parallax to repalce hero section

--- client/src/Pages/Website.jsx
+++ client/src/Pages/Website.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Hero from '../components/Hero/Hero'
 import EventsCard from '../components/EventsCard/EventsCard'
 import EventSlider from '../components/EventSlider/EventSlider'
+import Parallax from "../components/parallax/Parallax"
 
 const Website = () => {
     return (
         <div className="App">
-        <Hero/>
+        <Parallax type="portfolio" />
         </div>
     )
 }
