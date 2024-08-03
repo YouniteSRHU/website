5141de66d6c22261bb464dd61aa95bd4370fdd47
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-08-03 10:08:25 +0530

    fix/Optimised-api-calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,69 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import './App.css'
+// import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+// import "./App.css";
+// import Layout from "./components/Layout/Layout";
+// import Website from "./Pages/Website";
+// import { Suspense, useEffect } from "react";
+// import Youthfest from "./Pages/Youthfest/Youthfest";
+// import SingleEvent from "./Pages/SingleEvent/SingleEvent";
+// import Contact from "./Pages/Contact/Contact";
+// import AboutUs from "./Pages/AboutUs/AboutUs";
+// import { QueryClient, QueryClientProvider } from "react-query";
+// import { ReactQueryDevtools } from "react-query/devtools";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import News from "./Pages/News/News";
+// const ScrollToTop = () => {
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [location]);
+
+//   return null;
+// };
+// function App() {
+//   const queryClient = new QueryClient();
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <BrowserRouter>
+//         <ScrollToTop />
+//         <Suspense fallback={<div>Loading...</div>}>
+//           <Routes>
+//             <Route element={<Layout />}>
+//               <Route path="/" element={<Website />} />
+//               <Route path="/YUVOTSAV'24">
+//                 <Route index element={<Youthfest />} />
+//                 {/* <Route path=":id" element={<SingleEvent />} /> */}
+//                 <Route
+//                   path=":id"
+//                   element={<SingleEvent />}
+//                   loader={async ({ params }) => {
+//                     try {
+//                       const eventData = await getEvent(params.id);
+//                       return { event: eventData };
+//                     } catch (error) {
+//                       throw new Response("Not Found", { status: 404 });
+//                     }
+//                   }}
+//                 />
+//               </Route>
+//               <Route path="/CONTACT US" element={<Contact />} />
+//               <Route path="/ABOUT US" element={<AboutUs />} />
+//               <Route path="/NEWS" element={<News />} />
+//             </Route>
+//           </Routes>
+//         </Suspense>
+//       </BrowserRouter>
+//       <ToastContainer />
+//       <ReactQueryDevtools initialIsOpen={false} />
+//     </QueryClientProvider>
+//   );
+// }
+
+// export default App;
+
+import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom";
+import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Website from "./Pages/Website";
 import { Suspense, useEffect } from "react";
@@ -12,6 +76,8 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import News from "./Pages/News/News";
+import { getEvent } from "./utils/api"; // Make sure to import your getEvent function
+
 const ScrollToTop = () => {
   const location = useLocation();
 
@@ -21,31 +87,50 @@ const ScrollToTop = () => {
 
   return null;
 };
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Website /> },
+      {
+        path: "/YUVOTSAV'24",
+        children: [
+          { index: true, element: <Youthfest /> },
+          {
+            path: ":id",
+            element: <SingleEvent />,
+            loader: async ({ params }) => {
+
+              try {
+                const eventData = await getEvent(params.id);
+                return { eventData: eventData };
+              } catch (error) {
+                throw new Response("Not Found", { status: 404 });
+              }
+            },
+          },
+        ],
+      },
+      { path: "/CONTACT US", element: <Contact /> },
+      { path: "/ABOUT US", element: <AboutUs /> },
+      { path: "/NEWS", element: <News /> },
+    ],
+  },
+]);
+
 function App() {
   const queryClient = new QueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-    <ScrollToTop/>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<Website />} />
-                <Route path="/YUVOTSAV'24">
-                  <Route index element={<Youthfest />} />
-                  <Route path=":id" element={<SingleEvent />} />
-                </Route>
-                <Route path="/CONTACT US" element={<Contact />} />
-                <Route path="/ABOUT US" element={<AboutUs />} />
-                <Route path="/NEWS" element={<News />} />
-              </Route>
-            </Routes>
-          </Suspense>
-        </BrowserRouter> 
-        <ToastContainer />
-        <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-  )
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
--- client/src/Pages/SingleEvent/SingleEvent.jsx
+++ client/src/Pages/SingleEvent/SingleEvent.jsx
@@ -1,55 +1,102 @@
-import React from 'react'
-import EventHeader from '../../components/EventHeader/EventHeader'
-import './SingleEvent.css'
-import Warning from '../../components/Warning/Warning'
-import EventInfo from '../../components/EventInfo/EventInfo'
-import { useQuery } from "react-query";
-import { useLocation } from "react-router-dom";
-import { getEvent,getFest } from '../../utils/api'
-import { Hourglass } from "react-loader-spinner";
+// import React from 'react'
+// import EventHeader from '../../components/EventHeader/EventHeader'
+// import './SingleEvent.css'
+// import Warning from '../../components/Warning/Warning'
+// import EventInfo from '../../components/EventInfo/EventInfo'
+// import { useQuery } from "react-query";
+// import { useLocation } from "react-router-dom";
+// import { getEvent,getFest } from '../../utils/api'
+// import { Hourglass } from "react-loader-spinner";
+// const SingleEvent = () => {
+//     const { pathname } = useLocation();
+//     const id = pathname.split("/").slice(-1)[0];
+//     const { data, isLoading, isError } = useQuery(["event", id], () =>
+//         getEvent(id)
+//     );
+//     const { festdata } = useQuery(["fest"], () =>
+//     getFest()
+// );
+//     if (isLoading) {
+//         return (
+//             <div className="wrapper">
+//                 <div className="flexCenter paddings">
+//                     <Hourglass />
+//                 </div>
+//             </div>
+//         );
+//     }
+
+//     if (isError) {
+//         return (
+//             <div className="wrapper">
+//                 <div className="flexCenter paddings">
+//                     <span>Error while fetching the property details</span>
+//                 </div>
+//             </div>
+//         );
+//     }
+//     return (
+//         <>
+//             <Warning message={[`This event has same timing as ${data?.colliding_events}  `]} />
+//             <EventHeader eh_imageURl={data.images} eh_heading={data?.event_name} eh_desc={data?.event_desc} regLink={data?.registration_link} buttonText="Register Now!"/>
+//             <EventInfo
+//                 date={data?.event_datetime}
+//                 venue={data?.event_venue}
+//                 guidelines={data?.guidelines}
+//                 terms={data?.tc}
+//                 mapImage={data?.event_map}
+//                 phone={data?.contact_details}
+//                 brochureLink={data?.brochure_link}
+//             />
+//         </>
+//     )
+// }
+
+// export default SingleEvent
+
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import EventHeader from '../../components/EventHeader/EventHeader';
+import Warning from '../../components/Warning/Warning';
+import EventInfo from '../../components/EventInfo/EventInfo';
+import './SingleEvent.css';
+
 const SingleEvent = () => {
-    const { pathname } = useLocation();
-    const id = pathname.split("/").slice(-1)[0];
-    const { data, isLoading, isError } = useQuery(["event", id], () =>
-        getEvent(id)
-    );
-    const { festdata } = useQuery(["fest"], () =>
-    getFest()
-);
-    if (isLoading) {
+    const { eventData } = useLoaderData(); // ensuring consistent naming
+    if (!eventData) { 
+        console.error("Error: Event data not found"); // Adding more useful logging
         return (
             <div className="wrapper">
                 <div className="flexCenter paddings">
-                    <Hourglass />
+                    <span>Error while fetching the event details</span>
                 </div>
             </div>
         );
     }
 
-    if (isError) {
-        return (
-            <div className="wrapper">
-                <div className="flexCenter paddings">
-                    <span>Error while fetching the property details</span>
-                </div>
-            </div>
-        );
-    }
     return (
         <>
-            <Warning message={[`This event has same timing as ${data?.colliding_events}  `]} />
-            <EventHeader eh_imageURl={data.images} eh_heading={data?.event_name} eh_desc={data?.event_desc} regLink={data?.registration_link} buttonText="Register Now!"/>
+            {eventData.colliding_events && (
+                <Warning message={[`This event has the same timing as ${eventData.colliding_events}`]} />
+            )}
+            <EventHeader 
+                eh_imageURl={eventData.images || []} 
+                eh_heading={eventData.event_name || ''}
+                eh_desc={eventData.event_desc || ''}
+                regLink={eventData.registration_link || '#'}
+                buttonText="Register Now!"
+            />
             <EventInfo
-                date={data?.event_datetime}
-                venue={data?.event_venue}
-                guidelines={data?.guidelines}
-                terms={data?.tc}
-                mapImage={data?.event_map}
-                phone={data?.contact_details}
-                brochureLink={data?.brochure_link}
+                date={eventData.event_datetime || ''}
+                venue={eventData.event_venue || ''}
+                guidelines={eventData.guidelines || []}
+                terms={eventData.tc || []}
+                mapImage={eventData.event_map || ''}
+                phone={eventData.contact_details || ''}
+                brochureLink={eventData.brochure_link || '#'}
             />
         </>
-    )
-}
+    );
+};
 
-export default SingleEvent
+export default SingleEvent;
